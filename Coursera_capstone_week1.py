6d50cac3c1c0faa9ba2fec5d463d47e15f765a27
Aistė Kisielienė <Aiste.Kisieliene@esf.lt>
2019-12-09 21:53:00 +0200

    adds a python script that prints a message Hello Capstone Project Course!

---
+++ Coursera_capstone_week1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+print ("pandas imported")
+
+
+# In[2]:
+
+
+import numpy as np
+print ("Numpy imported")
+
+
+# Hello Capstone Project Course!
+
+# In[ ]:
+
+
+
+
